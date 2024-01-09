8a6bf5484dbeccbe6c3c9ea1e85658a76266bafb
ramir <ramir@dellNotebook>
2024-01-08 23:33:15 -0300

    Ajuste atribuição cumulativa

--- curso_programacao/src/curso_programacao/ProblemaExemplo.java
+++ curso_programacao/src/curso_programacao/ProblemaExemplo.java
@@ -14,7 +14,7 @@ public class ProblemaExemplo {
 		
 		double conta = 50.0;
 		if (minutos > 100) {
-			conta = conta + (minutos - 100) * 2.0;
+			conta += (minutos - 100) * 2.0;
 		}
 		
 		System.out.printf("Valor da conta = R$ %.2f\n", conta);
